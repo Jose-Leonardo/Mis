d2aac1d10d9cf4f66936c5602b1a8ecf31fdb016
José Leonardo <augl020225@gmail.com>
2022-09-22 12:39:48 -0700

    mi preimer ccodigo SQL

---
+++ Sql.sql
@@ -0,0 +1,22 @@
+insert into users(
+id,
+name,
+email,
+age,
+height,
+is_active
+) values (
+'3d26cd4b-b215-4d6b-987f-e85fbb6c31c1',
+'otro',
+'otro@gamil.com',
+21,
+1.60,
+false );
+--estge es un comentario
+--GET todos los usuarios
+
+select * from users;
+select id, name from users;
+select id, name from users where is_active = true;
+select id, name from users where is_active != true;
+select id, name from users where age = 21;
